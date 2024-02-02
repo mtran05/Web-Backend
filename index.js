c3ab6665bb12153104ce637f5a6a7d575c7fed0e
Mason <tranquocthinh2912@gmail.com>
2024-02-02 15:25:34 -0500

    updated CORS

--- index.js
+++ index.js
@@ -4,11 +4,20 @@ const app = express();
 // reference our own modules
 const academicProvider = require('./scripts/academic-provider.js');
 const academicHandler = require('./scripts/academic-router.js');
+
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 // handle requests for static resources
 //app.use('/static', express.static(path.join(__dirname, 'public')));
 academicHandler.handleAll(academicProvider, app);
 academicHandler.handleSingleTerm(academicProvider, app);
 academicHandler.handleNameSearch(academicProvider, app);
+
 // for anything else, display 404 errors
 app.use( (req,resp) => {
     resp.status(404).send('Unable to find the requested resource!');
